b685b60f5dcc53712135bb523ef9a8e7ec9b236a
vivek <vivek.koyani@gmail.com>
2020-04-24 16:04:58 +0530

    add header components

--- todoist_tut/src/App.js
+++ todoist_tut/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Header } from './components/layout/Header';
 
 export const App = () => {
   return (
     <div className="App">
-     <h1>Hello</h1>
+     <Header />
     </div>
   );
 }
