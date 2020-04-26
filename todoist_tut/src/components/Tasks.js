470572d8fe7daad067a797fddc4bb28e6a67c372
vivek <vivek.koyani@gmail.com>
2020-04-26 23:19:48 +0530

    create task

---
+++ todoist_tut/src/components/Tasks.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Tasks = () => {
+  const tasks = [];
+
+  const projectName = ``;
+
+  return (
+    <div className="tasks" data-testid="tasks">
+      <h2 data-testid="project-name">{projectName}</h2>
+      <ul className="task__list">
+        {tasks.map((task) => (
+          <li key={`${task.id}`}>
+            <Checkbox id={task.id} />
+            <span>{task.task}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
